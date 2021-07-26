a4f15724db582ec66be2a41e2055a72bcb0e7cd5
judes77 <judyphilip77@btinternet.com>
2021-07-26 16:22:18 +0100

    adds input and if-else statement

---
+++ guessing_game.py
@@ -0,0 +1,5 @@
+fav_animal = input("My favourite animal is either a cat, a dog or a horse.  Can you guess which one it is? ")
+if fav_animal == "dog":
+    print ("Well done!")
+else:
+    print("Not quite, try again!")
